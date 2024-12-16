92bf6ddc6dd6fdbf0021ded07245983abbd57daf
Devin Warrick <dev.warr@gmail.com>
2024-12-15 23:38:54 -0600

    removes unused files
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>

--- src/MovingSprite.ts
+++
@@ -1,9 +0,0 @@
-import { Sprite } from "pixi.js";
-import { Direction } from "./handleMovement";
-
-export class MovingSprite {
-  constructor(
-    public directionToMove: Direction,
-    public sprite: Sprite,
-  ) {}
-}
