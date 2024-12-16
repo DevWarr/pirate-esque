b9cc3eb42017e4fc31211c50c4e4c5da0aca8200
Devin Warrick <49497246+DevWarr@users.noreply.github.com>
2024-12-15 23:42:13 -0600

    Create initial ship controller (#2)
    * Creates Button class to hold info about player input
    
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>
    
    * Implements controller for multiple buttons
    
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>
    
    * Implements ship with ability to move through map
    
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>
    
    * removes unused files
    
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>
    
    * remove console.log
    
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>
    
    ---------
    
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>

--- src/Map.ts
+++ src/Map.ts
@@ -1,5 +1,5 @@
 import { Container, Sprite } from "pixi.js";
-import { PositionVector2 } from "./gameTypes";
+import { PositionVector2, SizeVector2 } from "./gameTypes";
 import { TextureKey, TextureManager } from "./TextureManager";
 
 /**
@@ -30,8 +30,18 @@ export const startingMapArray: UnderLayerTileType[][] = [
 export class Map {
   public static shipLocation: PositionVector2 = { x: 0, y: 0 };
 
-  private static getTexture(x: number, y: number, mapData: UnderLayerTileType[][]) {
-    if (mapData[y][x] === "_") {
+  public static tileMap: UnderLayerTileType[][] = [[]];
+
+  /** returns the size of the tileMap. Assumes the tileMap is a rectangle */
+  public static get tileMapSize(): SizeVector2 {
+    return {
+      w: Map.tileMap[0].length,
+      h: Map.tileMap.length,
+    };
+  }
+
+  private static getTexture(x: number, y: number) {
+    if (this.tileMap[y][x] === "_") {
       return TextureManager.getTexture(TextureKey.WATER);
     }
 
@@ -41,13 +51,16 @@ export class Map {
   /**
    * Takes a 2d array of UnderLayerTileType and creates sprites that can be added to the app stage.
    *
-   * NOTE: This doesn't add the map to the app stage itself
+   * NOTES:
+   *
+   * - This doesn't add the sprite 2d array to any containers
+   * - this.tileMap must be set first!
    * @param mapToGenerate
    */
-  public static buildSpriteMap(mapToGenerate: UnderLayerTileType[][]) {
-    return mapToGenerate.map((textureIndexArray, yPosition) =>
+  public static buildSpriteMap(): Sprite[][] {
+    return this.tileMap.map((textureIndexArray, yPosition) =>
       textureIndexArray.map((_, xPosition) => {
-        const texture = this.getTexture(xPosition, yPosition, mapToGenerate);
+        const texture = this.getTexture(xPosition, yPosition);
         return new Sprite({
           texture,
           x: 16 * xPosition,
@@ -55,6 +68,10 @@ export class Map {
         });
       }),
     );
+  }
+
+  public static isLand(position: PositionVector2): boolean {
+    return Map.tileMap[position.y][position.x] === "M";
   }
 
   /**
