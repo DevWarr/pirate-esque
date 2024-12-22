0fb63853669353b525808b723f6c6c6ee2935884
Devin Warrick <dev.warr@gmail.com>
2024-12-22 00:41:48 -0600

    Implement ItemManager
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,13 @@ import { TextureManager } from "./managers/TextureManager";
 import { TerrainManager, startingMapArray } from "./managers/TerrainManager";
 import { Controller } from "./controllers/Controller";
 import { Ship } from "./Ship";
+import { ItemManager } from "./managers/ItemManager";
 
 const app = new Application();
 await TextureManager.loadSpritesheet();
 await app.init({ width: 320, height: 240 });
 app.renderer.view.resolution = 3;
-
 document.getElementById("app")!.appendChild(app.canvas);
-
-let isPaused = false;
-const playerController = new Controller();
 
 const mapLayer = new Container();
 app.stage.addChild(mapLayer);
@@ -21,14 +18,29 @@ app.stage.addChild(mapLayer);
 TerrainManager.buildTerrainMap(startingMapArray);
 TerrainManager.updateContainerWithNewMapSprites(mapLayer);
 
+const hazardLayer = new Container();
+app.stage.addChild(hazardLayer);
+
+// HazardManager.buildSpriteMapFromMapTiles(startingMapArray);
+// HazardManager.updateContainerWithNewMapSprites(hazardLayer);
+
+const itemLayer = new Container();
+app.stage.addChild(itemLayer);
+
+ItemManager.buildItemMap(startingMapArray);
+ItemManager.updateContainerWithNewMapSprites(itemLayer);
+
 const shipLayer = new Container();
 app.stage.addChild(shipLayer);
 
+const playerController = new Controller();
 const ship = new Ship(playerController);
 ship.placeShipInContainer(shipLayer);
 
 const pauseLayer = new Container();
 app.stage.addChild(pauseLayer);
+
+let isPaused = false;
 const foreground = new Sprite(Texture.WHITE);
 foreground.width = app.canvas.width;
 foreground.height = app.canvas.height;
