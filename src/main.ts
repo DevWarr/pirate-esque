91e9e4111af9a36e7eff3c9ba8c2757838546aa7
Devin Warrick <dev.warr@gmail.com>
2024-12-22 20:12:09 -0600

    Implement base-game HUD
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -7,41 +7,47 @@ import { Ship } from "./Ship";
 import { ItemManager } from "./managers/ItemManager";
 import { HazardManager } from "./managers/HazardManager";
 import { startingMapArray } from "./mapLayout";
+import { HudLayer } from "./managers/HudManager";
 
 const app = new Application();
 await TextureManager.loadSpritesheet();
-await app.init({ width: 320, height: 240 });
+await app.init({ width: 320, height: 240 + 32 });
 app.renderer.view.resolution = 3;
 document.getElementById("app")!.appendChild(app.canvas);
 
-const mapLayer = new Container();
-app.stage.addChild(mapLayer);
+const hudLayer = new Container();
+hudLayer.height = 32;
+app.stage.addChild(hudLayer);
+HudLayer.buildHudLayer();
+HudLayer.addSpritesToHudLayer(hudLayer);
 
+const gameLayer = new Container();
+gameLayer.y = 32;
+app.stage.addChild(gameLayer);
+
+const mapLayer = new Container();
+gameLayer.addChild(mapLayer);
 TerrainManager.buildTerrainMap(startingMapArray);
 TerrainManager.updateContainerWithNewMapSprites(mapLayer);
 
 const hazardLayer = new Container();
-app.stage.addChild(hazardLayer);
-
+gameLayer.addChild(hazardLayer);
 HazardManager.buildHazardMap(startingMapArray);
 HazardManager.updateContainerWithNewMapSprites(hazardLayer);
 
 const itemLayer = new Container();
-app.stage.addChild(itemLayer);
-
+gameLayer.addChild(itemLayer);
 ItemManager.buildItemMap(startingMapArray);
 ItemManager.updateContainerWithNewMapSprites(itemLayer);
 
 const shipLayer = new Container();
-app.stage.addChild(shipLayer);
-
+gameLayer.addChild(shipLayer);
 const playerController = new Controller();
 const ship = new Ship(playerController);
 ship.placeShipInContainer(shipLayer);
 
 const pauseLayer = new Container();
 app.stage.addChild(pauseLayer);
-
 let isPaused = false;
 const foreground = new Sprite(Texture.WHITE);
 foreground.width = app.canvas.width;
