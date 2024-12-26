739f6aefda77697e9d8e8c5dee20d248cc675ef4
Devin Warrick <dev.warr@gmail.com>
2024-12-26 13:14:43 -0600

    Update Position vector to include hard-coded directions, and an interface
    Signed-off-by: Devin Warrick <dev.warr@gmail.com>

--- src/models/PositionVector2.ts
+++ src/models/PositionVector2.ts
@@ -1,6 +1,11 @@
 export type SerializedPositionVector2 = `${number},${number}`;
 
-export class PositionVector2 {
+export interface IPositionVector2 {
+  x: number;
+  y: number;
+}
+
+export class PositionVector2 implements IPositionVector2 {
   constructor(
     private _x: number,
     private _y: number,
@@ -16,5 +21,25 @@ export class PositionVector2 {
 
   get serializedPosition(): SerializedPositionVector2 {
     return `${this._x},${this._y}`;
+  }
+
+  static get LEFT(): PositionVector2 {
+    return new PositionVector2(-1, 0);
+  }
+
+  static get RIGHT(): PositionVector2 {
+    return new PositionVector2(1, 0);
+  }
+
+  static get UP(): PositionVector2 {
+    return new PositionVector2(0, -1);
+  }
+
+  static get DOWN(): PositionVector2 {
+    return new PositionVector2(0, 1);
+  }
+
+  static add(a: IPositionVector2, b: IPositionVector2): PositionVector2 {
+    return new PositionVector2(a.x + b.x, a.y + b.y);
   }
 }
